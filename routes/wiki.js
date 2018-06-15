90f46e49ada9752c717265456fafa302ce0d4d13
Hawa Sako <Hawa.d.sako@gmail.com>
2018-06-15 17:13:27 -0400

    final

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 const bodyParser = require('body-parser')
 const layout = require('../views/layout')
-const addPage = require('../views/addPage')
+const wikipage = require('../views/wikipage')
 
 
 router.use(bodyParser.urlencoded({extended: true}));
@@ -11,12 +11,49 @@ router.get('/', (req, res, next) => {
 	res.send(layout(''))
 });
 
-router.post('/', (req,res) => {
-	res.send(layout(req.body.email));
-});
-
 router.get('/add', (req, res, next) => {
 	res.send(addPage());
+});
+
+const { Page } = require("../models");
+const { addPage } = require("../views");
+
+router.post('/', async(req, res, next) => {
+  const page = new Page({
+    title: req.body.title, //in a post (made in HTML form) the req is the json object of what we input
+    content: req.body.content
+  });
+
+
+  function generateSlug (title) {
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
+Page.beforeValidate((pageInstance)=> {
+	pageInstance.slug = generateSlug(pageInstance.title)
+});
+
+  try {
+  	console.log(page)
+    await page.save();
+    res.redirect(`/wiki/${page.slug}`);
+  } catch (error) { next(error)
+  }
+});
+
+  router.get('/:slug', async (req, res, next) => {
+	  try{
+	  	const page = await Page.findOne({
+	  		where: {
+	  			slug: req.params.slug
+	  		}
+	  	});
+	  	res.send(wikipage(page));
+
+	  }catch (error) {next(error)}
+
 });
 
 module.exports = router
